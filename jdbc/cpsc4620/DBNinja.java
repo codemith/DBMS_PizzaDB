b49ece652fc805552fb1e3bc3dea1ce5bcca42d7
codemith <mithileshb116@gmail.com>
2024-12-04 20:04:52 -0500

    backtrac

--- jdbc/cpsc4620/DBNinja.java
+++ jdbc/cpsc4620/DBNinja.java
@@ -194,6 +194,8 @@ public final class DBNinja {
 		 *
 		 */
 
+//		return -1;
+
 		try {
 			connect_to_db();
 			if(conn != null){
@@ -1381,7 +1383,7 @@ public final class DBNinja {
 
 		connect_to_db(); // Connect to the database
 
-		String query = "SELECT * FROM ToppingPopularity;
+		String query = "SELECT * FROM ToppingPopularity ORDER BY `ToppingCount` DESC, 'Topping' ASC";
 
 		Statement stmt = conn.createStatement();
 		ResultSet rs = stmt.executeQuery(query);
