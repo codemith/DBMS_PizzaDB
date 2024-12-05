ab3d666602deec02ebde74baa9bc524f7277ce36
codemith <mithileshb116@gmail.com>
2024-12-04 23:39:55 -0500

    test

--- jdbc/sql/CreateSPs.sql
+++ jdbc/sql/CreateSPs.sql
@@ -2,21 +2,34 @@ USE PizzaDB;
 
 DELIMITER //
 
--- Stored Procedure 1
+CREATE FUNCTION GetCustomerOrderCount(customer_id INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE order_count INT;
+    SELECT COUNT(*)
+    INTO order_count
+    FROM ordertable
+    WHERE customer_CustID = customer_id;
+    RETURN order_count;
+END //
+DELIMITER ;
+
+
+DELIMITER //
+-- sp 1
 CREATE PROCEDURE ADDCUSTOMER(IN FNAME VARCHAR(30), IN LNAME VARCHAR(30), IN PHONENUM VARCHAR(30))
 BEGIN
     INSERT INTO customer (customer_FName, customer_LName, customer_PhoneNum)
     VALUES (FNAME, LNAME, PHONENUM);
 end //
-
--- Stored Procedure 2
+-- Sp 2
 CREATE PROCEDURE CREATEORDER(IN CUSTID INT, IN ORDERTYPE VARCHAR(30), IN ORDERDATETIME DATETIME, IN CUSTPRICE DECIMAL(5,2), IN BUSPRICE DECIMAL(5,2), IN ORDERCOMPLETE TINYINT(1))
 BEGIN
     INSERT INTO ordertable (customer_CustID, ordertable_OrderType, ordertable_OrderDateTime, ordertable_CustPrice, ordertable_BusPrice, ordertable_isComplete)
     VALUES (CUSTID, ORDERTYPE, ORDERDATETIME, CUSTPRICE, BUSPRICE, ORDERCOMPLETE);
 end //
-
--- Stored Function 1
+-- funct 1
 CREATE FUNCTION CUSTTOTAL(ORDERID INT) RETURNS DECIMAL(5,2)
 DETERMINISTIC
 BEGIN
@@ -26,8 +39,7 @@ BEGIN
     WHERE ordertable_OrderID = ORDERID;
     RETURN TOTAL;
 end //
-
--- Stored Function 2
+-- funct 2
 CREATE FUNCTION BUSTOTAL(ORDERID INT) RETURNS DECIMAL(5,2)
 DETERMINISTIC
 BEGIN
@@ -38,7 +50,7 @@ BEGIN
     RETURN TOTAL;
 end //
 
--- Update Trigger 1
+-- up trigger 1
 CREATE TRIGGER PIZZASTATUS AFTER UPDATE ON pizza
 FOR EACH ROW
 BEGIN
@@ -49,7 +61,7 @@ BEGIN
     end if;
 end //
 
--- Update Trigger 2
+-- up trigger 2
 CREATE TRIGGER BEFOREPIZZASTATUS
 AFTER UPDATE ON pizza
 FOR EACH ROW
@@ -61,7 +73,7 @@ BEGIN
     end if;
 end //
 
--- Insert Trigger 1
+-- insert trigger 1
 CREATE TRIGGER DEFORDERTYPE
 BEFORE INSERT ON ordertable
 FOR EACH ROW
@@ -71,7 +83,7 @@ FOR EACH ROW
         end if;
     end //
 
--- Insert Trigger 2
+-- insert trigger 2
 CREATE TRIGGER DEFTOPPINGINV
 BEFORE INSERT ON topping
 FOR EACH ROW
